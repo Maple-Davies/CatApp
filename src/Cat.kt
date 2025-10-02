d26ab885edb9ec37eb417b208a22b2a554eb4e96
Michael Davies <michaeldavies924@gmail.com>
2025-10-02 22:54:26 +0100

    Added eat() method and created two new cats

--- src/Cat.kt
+++ src/Cat.kt
@@ -7,4 +7,7 @@ class Cat (val name: String, var age: Int, var weight: Int) {
     override fun toString() : String {
         return "Name: $name Age: $age"
     }
+    fun eat() {
+        weight++
+    }
 }
